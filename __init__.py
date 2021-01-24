29c16492fed486c8ea9300a4345c621c5351dff7
Shams Kitz <dustractor@gmail.com>
2021-01-24 12:52:26 -0600

    updated
    the method for splitting and joining areas
    A is the context.area where the action initiated
    B is the area which is found on the right side (currently hardcoded)
    A's x plus it's width plus ???three???for the first value of the cursor tuple
    A's x plus half the value of it's height for the second part of the cursor tuple
    cursor tuple passed as described seemed to be the only way to get finished not cancelled the operation.
    toggle fullscreen and back: the first toggle, there is no longer the area A which initiated the action and  so B is passed in a fake context override as the area of the context
    the second toggle is the final step and does not seem to require a fake context

--- __init__.py
+++ __init__.py
@@ -1,10 +1,28 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 
 bl_info = {
         "name": "Areatype Split (3D View/UV Editor)",
-        "description":"This example adds a button which toggles a split of an area with another.",
+        "description":"a button which toggles a split of an area with another.",
         "author":"dustractor@gmail.com",
-        "version":(0,1),
-        "blender":(2,65,0),
+        "version":(0,2),
+        "blender":(2,80,0),
         "location":"Prepended to the header of the viewport.",
         "warning":"",
         "wiki_url":"",
@@ -13,53 +31,51 @@ bl_info = {
 
 import bpy
 
+_area_type_enums = [
+    _.identifier for _ in bpy.types.Area.bl_rna.properties["type"].enum_items]
 
 class AREATYPE_OT_split(bpy.types.Operator):
     bl_idname = "areatype.splitview"
     bl_label = "areatype.splitview"
+    splittype: bpy.props.EnumProperty(
+        items=[(_,_,_) for _ in _area_type_enums],
+        default="IMAGE_EDITOR")
+    direction: bpy.props.EnumProperty(
+        items=[(_,_,_) for _ in ("HORIZONTAL","VERTICAL")],
+        default="VERTICAL")
     def execute(self,context):
-        thisarea = context.area
-        otherarea = None
-        tgxvalue = thisarea.x + thisarea.width + 1
-        thistype = context.area.type
-        arealist = list(context.screen.areas)
-        for area in context.screen.areas:
-            if area == thisarea:
-                continue
-            elif area.x == tgxvalue and area.y == thisarea.y:
-                otherarea = area
-                break
-        if otherarea:
-            bpy.ops.screen.area_join(min_x=thisarea.x,min_y=thisarea.y,max_x=otherarea.x,max_y=otherarea.y)
-            bpy.ops.screen.screen_full_area()
-            bpy.ops.screen.screen_full_area()
-            return {"FINISHED"}
+        A = context.area
+        _type = A.type
+        rs = [_ for _ in context.screen.areas if _.x > A.x and _.y == A.y]
+        if not rs:
+            A.type = self.splittype
+            bpy.ops.screen.area_split(direction=self.direction)
+            B = context.screen.areas[-1]
+            B.type = _type
         else:
-            context.area.type = "IMAGE_EDITOR"
-            areax = None
-            bpy.ops.screen.area_split(direction="VERTICAL")
-            for area in context.screen.areas:
-                if area not in arealist:
-                    areax = area
-                    break
-            if areax:
-                areax.type = thistype
-                return {"FINISHED"}
-        return {"CANCELLED"}
+            B = rs[0]
+            c = (A.x+A.width+3,A.x+A.height//2)
+            bpy.ops.screen.area_join(cursor=c)
+            fakectx = dict()
+            B.type = _type
+            fakectx["area"] = B
+            bpy.ops.screen.screen_full_area(fakectx)
+            bpy.ops.screen.screen_full_area()
+        return {"FINISHED"}
 
 
 def viewdraw(self,context):
     layout = self.layout
-    layout.operator("areatype.splitview",text="",icon="COLOR_BLUE")
+    op = layout.operator("areatype.splitview",text="",icon="IMAGE")
+    op.splittype = "IMAGE_EDITOR"
 
 
 def register():
     bpy.types.VIEW3D_HT_header.prepend(viewdraw)
-    bpy.utils.register_module(__name__)
+    bpy.utils.register_class(AREATYPE_OT_split)
 
 
 def unregister():
     bpy.types.VIEW3D_HT_header.remove(viewdraw)
-    bpy.utils.unregister_module(__name__)
-    
+    bpy.utils.unregister_class(AREATYPE_OT_split)
 
